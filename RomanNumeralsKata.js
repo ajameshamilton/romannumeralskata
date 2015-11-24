4781df26867ba8bfb8fd344108982ef90c39a6a2
James Hamilton <ajhworkacct@gmail.com>
2015-11-24 11:57:20 -0500

    Added decision function to determine which conversion and error message if invalid input

--- RomanNumeralsKata.js
+++ RomanNumeralsKata.js
@@ -90,6 +90,17 @@ All encoder tests successful. */
 
 
 //ADDING DECODER function
+function decision(input){
+  if (input == parseInt(input)){
+    return cA2R(input);
+  } else if (input.search(/[cdilmx/gi) != -1){
+    return cR2A(input);
+  } else {
+    return " I'm sorry, that's not a valid input. Please enter a value in either Roman or Arabic numerals."
+  }
+
+}
+
 
 function cR2A(roman){
   var letters= {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1 }
